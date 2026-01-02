030cbc07bc155c19495bb8636a1133f711e98019
Lanre Afolabi <olamarionafol@gmail.com>
2026-01-02 15:58:56 -0600

    Set up header, weathercards, modals and footer

---
+++ se_project_react/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./Footer.css";
+function Footer(){
+    return <footer className="footer">
+            <p className="footer__name">Developed by Lanre Afolabi</p>
+             <p className="footer__date">2025</p>
+    </footer>;}
+
+export default Footer;
